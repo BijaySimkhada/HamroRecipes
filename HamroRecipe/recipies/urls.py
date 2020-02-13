beb71304d30ca38d99dfc1ba128d4422d0c4688d
sagarmishra07 <sagarmishraswor@gmail.com>
2020-02-13 20:01:14 +0545

    contact page + institutes layouts on going

--- HamroRecipe/recipies/urls.py
+++ HamroRecipe/recipies/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.showIndex, name='home'),
     path('contact', views.showContact, name="contact"),
+    path('institutes', views.showInstitutes, name="institutes"),
     path('recipe', views.showRecipe, name="recipe"),
 ]
